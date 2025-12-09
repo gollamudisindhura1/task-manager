8ed9b52bb368efbefb643cd6a90924d1a9f51d8b
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2025-12-08 21:06:23 -0500

    Initial commit

--- src/components/TaskFilter/TaskFilter.tsx
+++
@@ -1,9 +0,0 @@
-import type { TaskStatus } from "../TaskList/TaskList";
-
-// types/index.ts
-export interface TaskFilterProps {
-  onFilterChange: (filters: {
-    status?: TaskStatus;
-    priority?: 'low' | 'medium' | 'high';
-  }) => void;
-}
--- src/components/TaskItem/TaskItem.tsx
+++
@@ -1,8 +0,0 @@
-import type { Task, TaskStatus } from "../TaskList/TaskList";
-
-// types/index.ts
-export interface TaskItemProps {
-  task: Task;
-  onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
-  onDelete: (taskId: string) => void;
-}
